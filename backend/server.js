22e92f4241f1a0119e6e345edc04d1a5f36397fb
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-16 22:17:15 -0400

    server started

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(`Hello Backend`);
+})
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server running at port ${PORT}`);
+})
+
