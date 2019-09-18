d65410330239e654f071ed4aa758a575339951fc
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-17 20:40:54 -0400

    mongodb connceted successfully

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
+const uri = process.env.DATABASE;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log(`Mongo db succeessfully connected`);
+})
+
+
 //middleware
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
