566e1c53d9fb89f25662fbe98349426ac6e7446f
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-18 22:36:56 -0400

    component updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from './components/Navbar';
+import ExerciseList from './components/ExerciseList';
+import EditExercise from './components/EditExercise';
+import CreateExercise from './components/CreateExercise';
+import CreateUser from './components/CreateUser';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React This is functional react app</h1>
-    </div>
+    <Router>
+      <Navbar/>
+      <br/>
+      <Route path="/" exact component ={ExerciseList}/>
+      <Route path="/edit/:id"  component ={EditExercise}/>
+      <Route path="/create"  component ={CreateExercise}/>
+      <Route path="/user"  component ={CreateUser}/>
+
+    </Router>
   );
 }
 
