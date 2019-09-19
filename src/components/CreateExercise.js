368259cc191c19b0108bade4d6cd5653ad755f4a
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-19 19:07:45 -0400

    creating exercise component

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -12,6 +12,40 @@ export default class CreateExercise extends Component {
             users: []
         }
     }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        })
+    };
+    onChangeDuration(e) {
+        this.setState({
+            duration: e.target.value
+        })
+    };
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        })
+    };
+
+    onSubmit(e) {
+        e.preventDefault();
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(exercise);
+    }
     render() {
         return (
         <nav className="navbar navbar-dark bg-dark navbar-expand">
