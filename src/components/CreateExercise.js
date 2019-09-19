da41a06b16cd2a281e87497f62211409a662b218
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-19 18:58:41 -0400

    creating component

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class CreateExercise extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
     render() {
         return (
         <nav className="navbar navbar-dark bg-dark navbar-expand">
