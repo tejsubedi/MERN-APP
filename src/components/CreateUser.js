763fc4f825701d897a93e287df6765e9de70759b
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-19 21:25:09 -0400

    added exercise list

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
     constructor(props) {
@@ -26,9 +26,16 @@ export default class CreateUser extends Component {
             
         }
         console.log(user);
+
+        axios.post('http://localhost:5000/users/add', user) 
+            .then(res => console.log(res.data));
+            
+
         this.setState({
             username:'',
         })
+
+
     };
 
 
--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ExerciseList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.deleteExercise = this.deleteExercise.bind(this);
+        
+
+    }
+
     render() {
         return (
         <nav className="navbar navbar-dark bg-dark navbar-expand">
